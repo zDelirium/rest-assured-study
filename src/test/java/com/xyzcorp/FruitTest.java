4efc35b982db2817e4011d302f964408dfdeba4c
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-02-02 13:26:54 -0700

    Added a Post to the Fruit Test

--- src/test/java/com/xyzcorp/FruitTest.java
+++ src/test/java/com/xyzcorp/FruitTest.java
@@ -1,6 +1,7 @@
 package com.xyzcorp;
 
 import io.restassured.http.ContentType;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -20,5 +21,25 @@ public class FruitTest {
                 .then()
                 .assertThat()
                 .body("[0].description", equalTo("Winter fruit"));
+    }
+
+    @Test
+    public void testPostNewFruit() {
+        JSONObject bananaObject = new JSONObject()
+                .put("name", "banana")
+                .put("description", "a delicious treat");
+
+        System.out.println(bananaObject);
+
+        given()
+                .relaxedHTTPSValidation()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body(bananaObject.toString())
+                .when()
+                .post("https://staging.tiered-planet.net/mild-temper/fruits")
+                .then()
+                .assertThat()
+                .statusCode(200);
     }
 }
