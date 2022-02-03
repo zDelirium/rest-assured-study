ab4cfa4ca707f8969d6413c866eef158b5f4248a
Gia-Khang Ernest Nguyen <ernest.nguyen@cgi.com>
2022-02-03 08:57:23 -0500

    Added Legumes test for get Zucchini description

---
+++ src/test/java/com/xyzcorp/LegumesTest.java
@@ -0,0 +1,22 @@
+package com.xyzcorp;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class LegumesTest {
+
+    @Test
+    public void testGetLegumesZucchini() {
+        given()
+                .relaxedHTTPSValidation()
+                .accept(ContentType.JSON)
+                .when()
+                .get("https://staging.tiered-planet.net/mild-temper/legumes")
+                .then()
+                .assertThat()
+                .body("[1].description", equalTo("Summer squash"));
+    }
+}
